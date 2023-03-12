c7c1f969934593c5ce0ce916f0663accd815a7a4
Matthew Greco <mgreco2112@tutanota.com>
2023-03-12 00:04:56 -0500

    solved CPP hackerrank challenge

---
+++ io read/io read.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int one, two, three;
+    
+    cin >> one >> two >> three;
+    
+    int sum = one + two + three;
+    
+    cout << sum;
+     
+    return 0;
+}
